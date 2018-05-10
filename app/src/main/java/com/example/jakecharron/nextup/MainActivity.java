3a48013a97e5ee3954ded7d32a6507c1a498b66d
Jake Charron <jcharron0306@gmail.com>
2018-05-10 18:48:59 -0400

    Backup

--- app/src/main/java/com/example/jakecharron/nextup/MainActivity.java
+++ app/src/main/java/com/example/jakecharron/nextup/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.jakecharron.nextup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Starting Screen");
+        Button todoB = findViewById(R.id.todo);
+        todoB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { startActivity(new Intent(MainActivity.this, todo.class));            }
+        });
     }
 }
+
+
+
+
---
+++ app/src/main/java/com/example/jakecharron/nextup/completed.java
@@ -0,0 +1,14 @@
+package com.example.jakecharron.nextup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class completed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.completed);
+    }
+}
